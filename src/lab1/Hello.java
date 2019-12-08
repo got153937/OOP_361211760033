b5e8b46be12f30da0806d1fe65802ec34174ea27
GOT <amppatcharin2541@gmail.com>
2019-12-08 11:01:29 +0700

    Initial commit

---
+++ src/lab1/Hello.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World,I am Got");
+
+
+
+
+
+
+
+
+
+    }//main
+
+}//class
